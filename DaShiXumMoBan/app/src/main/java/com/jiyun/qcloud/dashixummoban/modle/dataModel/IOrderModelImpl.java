7de4a4e02b1f00ef936d8e3bcbe7ee7c9bde747a
admin <332151087@qq.com>
2017-08-26 11:16:13 +0800

    老王

--- DaShiXumMoBan/app/src/main/java/com/jiyun/qcloud/dashixummoban/modle/dataModel/IOrderModelImpl.java
+++ DaShiXumMoBan/app/src/main/java/com/jiyun/qcloud/dashixummoban/modle/dataModel/IOrderModelImpl.java
@@ -1,6 +1,7 @@
 package com.jiyun.qcloud.dashixummoban.modle.dataModel;
 
 import com.jiyun.qcloud.dashixummoban.base.BaseBean;
+import com.jiyun.qcloud.dashixummoban.config.Urls;
 import com.jiyun.qcloud.dashixummoban.modle.net.callback.NetWorkCallBack;
 
 /**
@@ -10,6 +11,6 @@ import com.jiyun.qcloud.dashixummoban.modle.net.callback.NetWorkCallBack;
 public class IOrderModelImpl implements IHomeModel {
     @Override
     public void loadHomeList(NetWorkCallBack<BaseBean> callback) {
-       // iHttp.get(Urls.ORDER,null,callback);
+        iHttp.get(Urls.ORDER,null,callback);
     }
 }
